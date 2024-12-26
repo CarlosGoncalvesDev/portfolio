63d2590c9aabed417bb6226c5319622cae63ac1a
carlos.goncalves <carloseduardo05.dev@gmail.com>
2024-12-26 16:22:19 -0300

    Adicionando um projeto

--- script.js
+++ script.js
@@ -12,9 +12,7 @@ const navLinks = document.querySelectorAll(".navbar a");
 
 // Função para definir o item ativo
 function setActiveLink() {
-  navLinks.forEach((link) => {
-    link.classList.remove("active");
-  });
+  navLinks.forEach((link) => link.classList.remove("active"));
   this.classList.add("active");
 }
 
@@ -25,49 +23,46 @@ navLinks.forEach((link) => {
 
 // Função para voltar ao estado "Home" ao clicar no logo
 const logo = document.querySelector(".logo");
-
 logo.addEventListener("click", () => {
-  navLinks.forEach((link) => {
-    link.classList.remove("active");
-  });
+  navLinks.forEach((link) => link.classList.remove("active"));
   const homeLink = document.querySelector('.navbar a[href="#home"]');
-  homeLink.classList.add("active");
+  if (homeLink) homeLink.classList.add("active");
 });
 
-
+// Carousel
 const carouselItems = document.querySelectorAll(".carousel-item");
 let currentIndex = 0;
 
 // Função para mostrar o item atual
 function showCurrentItem() {
   carouselItems.forEach((item, index) => {
-    item.classList.remove("active");
+    item.classList.remove("active"); // Remove a classe 'active' de todos os itens
     if (index === currentIndex) {
-      item.classList.add("active");
+      item.classList.add("active"); // Adiciona a classe 'active' ao item atual
     }
   });
 }
 
 // Função para ir para o próximo item
 function nextItem() {
-  currentIndex = (currentIndex + 1) % carouselItems.length;
-  showCurrentItem();
+  currentIndex = (currentIndex + 1) % carouselItems.length; // Avança para o próximo índice
+  showCurrentItem(); // Atualiza a exibição do carousel
 }
 
 // Função para ir para o item anterior
 function prevItem() {
-  currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
-  showCurrentItem();
+  currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length; // Retrocede para o índice anterior
+  showCurrentItem(); // Atualiza a exibição do carousel
 }
 
-// Adiciona eventos aos botões
+// Adiciona eventos aos botões do carousel
 document.getElementById("nextBtn").addEventListener("click", nextItem);
 document.getElementById("prevBtn").addEventListener("click", prevItem);
 
 // Opcional: adicionar rotação automática
 setInterval(nextItem, 5000); // Troca de item a cada 5 segundos
 
-// Obter todas as seções e os links do menu
+// Observando seções para marcar o link ativo no menu
 const sections = document.querySelectorAll("section");
 const options = {
   threshold: 0.6, // Percentual da seção visível na tela
